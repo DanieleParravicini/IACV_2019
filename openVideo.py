fix an error: shared object in numpy array caused sharing of variance across pixels

openVideo.py
@@ -6,6 +6,7 @@ import time
 #import function for normal distribution
 from scipy.stats import norm
 import math  
+import threading
 import matplotlib.pyplot as plt
 
 verbose = False
@@ -14,15 +15,10 @@ class GaussianMixtureModelPixel:
   LARGE_VARIANCE  = 20
   MATCH_THRESHOLD = 2.5
   ALPHA           = 0.01
-  LOW_WEIGHT      = 0.001
+  LOW_WEIGHT      = 0.01
   N = 0
   T = 0.7
   EPSILON = 0.001
-  
-
-  variances = []
-  averages  = []
-  weights   = []
   
   # constructor   
   def __init__(self,n = 3):  
@@ -31,19 +27,26 @@ class GaussianMixtureModelPixel:
     self.weights      = []
     self.N            = n
 
-  def update(self, new_x):
-    if verbose : 
-      print('ENTERING UPDATE:')
-      for i in range(len(self.averages)):
-        print('gaussian #',i, 
-        'w/var', self.weights[i]/(self.variances[i]+self.EPSILON), 
-        ' avg: ', self.averages[i], 
-        ' var: ', self.variances[i], 
-        'weight', self.weights[i] )
+  def renormalize_weights(self):
+    weights_tot  = sum(self.weights)
+    self.weights = list(map(lambda w: w/weights_tot, self.weights))
 
+  def background_index(self):
+    cumulative_w = 0
+    b_index = 0
+    while( cumulative_w < self.T and b_index < len(self.weights)):
+      cumulative_w += self.weights[b_index]
+      b_index+=1
+
+    return b_index -1
+
+  def update(self, new_x):
+    new_x = int(new_x)
     i = self.match(new_x)
 
     if verbose : 
+      print('ENTERING UPDATE:')
+      print_stats()
       print('gaussian matched by',new_x, ':', i)
     
     #is match found?
@@ -67,20 +70,21 @@ class GaussianMixtureModelPixel:
       self.variances[i] = self.LARGE_VARIANCE
     else :
       #if a match is found we have to modify the current values of the distributions
-      #rho = self.ALPHA*norm.pdf(new_x, self.averages [i], math.sqrt(self.variances[i]) )
-      rho = self.ALPHA 
+      rho = self.ALPHA#*norm.pdf(new_x, self.averages [i], math.sqrt(self.variances[i]) )
+      #rho  = self.ALPHA /max([ self.weights[i] , self.ALPHA])
       if verbose : 
         print('rho:',rho)
-      self.averages [i] = self.averages[i]   + rho   * (new_x - self.averages[i])
-      self.variances[i] = (self.variances[i] + rho   * ((new_x - self.averages[i])**2 - self.variances[i] ) )
+      self.averages [i] =  int(self.averages[i]   + rho   *  (new_x - self.averages[i]))
+      new_variance = int(self.variances[i]  + rho   * ((new_x - self.averages[i])**2 - self.variances[i] ) )
+      new_variance = max(new_variance, 1)
+      assert(new_variance >= 1)
+      self.variances[i] = new_variance
+
     #update weights
-    for j in range(len(self.weights)):
-      self.weights  [j] = (1 - self.ALPHA) * self.weights[j] + self.ALPHA * (j==i)
-    #renormalize weights
-    sum_weights  = sum(self.weights)
-    self.weights = list(map(lambda x: x/ sum_weights, self.weights))
+    self.weights = list(map(lambda j,w : w + self.ALPHA*( j==i - w), range(len(self.weights)), self.weights ))
+    self.renormalize_weights()
+
     #reorder according to self.weights[.]/self.variance[.]
-    
     weight     = self.weights   [i]
     average    = self.averages  [i]
     variance   = self.variances [i]
@@ -98,32 +102,48 @@ class GaussianMixtureModelPixel:
     self.averages.insert(j, average)
     self.variances.insert(j, variance)
 
+
     # the given pixel belongs to background if it
     # falls over T cumulative percentage in the 
     # gaussian list.
-    
-    is_background = sum(self.weights[:i]) <= self.T
+    b_index = self.background_index()
+    is_background = (i == b_index)
+
     if verbose:
       print('BEFORE LEAVING UPDATE:')
-      for i in range(len(self.averages)):
-        print('gaussian #',i, 
-        'w/var', self.weights[i]/(self.variances[i]+self.EPSILON), 
-        ' avg: ', self.averages[i], 
-        ' var: ', self.variances[i], 
-        'weight', self.weights[i] )
-    
+      print_stats()
+      print('is background: ', is_background)
+
     return not is_background
   
   #the index of the distribution matched or -1
   def match(self, x):
     
     for i in range(len(self.variances)):
-      
-      if( self.averages[i] - self.MATCH_THRESHOLD*math.sqrt(self.variances[i]) <  x   and
-          self.averages[i] + self.MATCH_THRESHOLD*math.sqrt(self.variances[i]) >= x  ):
+      if( abs(int(x)- self.averages[i]) <= self.MATCH_THRESHOLD*math.sqrt(self.variances[i]) ):
           return i
     return -1
+  
+  def print_stats(self):
+    for i in range(len(self.averages)):
+        print('gaussian #',i, 
+        'w/var', self.weights[i]/(self.variances[i]+self.EPSILON), 
+        ' avg: ', self.averages[i], 
+        ' var: ', self.variances[i], 
+        'weight', self.weights[i] )
+      
 
+def update_background(thread_id, num_thread, frame, state, output):
+  
+  tot_element = frame.shape[0]*frame.shape[1]
+  num_element = math.ceil(tot_element/num_thread) 
+  start = num_element*(thread_id-1) 
+  end   = start+num_element
+  for i in range(start,end):
+    y = i//frame.shape[1]
+    x = i % frame.shape[1]
+    output[y,x] = state[y][x].update(frame[y,x,0])
+  
 state = None
 stats = {}
 # Create a VideoCapture object and read from input file
@@ -140,24 +160,40 @@ while(cap.isOpened()):
   
   # Capture frame-by-frame
   ret, frame = cap.read()
-  #
-  print('frame:' , nr_frame , 'frame dimension ', frame.shape)
+  print(nr_frame, frame.shape)
   nr_frame +=1 
   if ret == True:
     # Display the resulting frame
-    
     test = np.full(frame.shape[:2],0)
 
     #fill state for the first time
     if state is None:
-      state = np.full(frame.shape[:2],GaussianMixtureModelPixel(4))
+      state = []
+      for i in range(frame.shape[0]):
+        state.append([])
+        for j in range(frame.shape[1]):
+          state[i].append(GaussianMixtureModelPixel(3))
+    
+
     #update
-    for i in range(100,200):#range(frame.shape[0]//2):
+    nr_threads = 32
+    threads = []
+    unshaped_frame = np.reshape(frame,-1)
+    
+    for i in range(nr_threads):
+      x = threading.Thread(target=update_background, args=(i, nr_threads, frame, state, test))
+      x.start()
+      threads.append(x)
+    
+    for t in threads:
+      t.join()
+
+    '''for i in range(100,200):#range(frame.shape[0]//2):
       for j in range(100,200):#range(frame.shape[1]//2):
         if((i,j) not in stats ):
           stats[(i,j)] = []
         stats[(i,j)].append(frame[i,j,0])
-        test[i,j] = state[i,j].update(frame[i,j,0])
+        test[i,j] = state[i][j].update(frame[i,j,0])'''
     
     test = test.astype(np.uint8)  #convert to an unsigned byte
     test*=255
@@ -167,7 +203,7 @@ while(cap.isOpened()):
                               np.expand_dims(test, axis=2)
                               
                               ], axis=2)
-    print(result.shape)
+    #print(result.shape)
     cv2.imshow('result',result)
     if(verbose):
       time.sleep(1)