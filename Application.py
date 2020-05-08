Add possibility to indicate error in the form of standard deviation to be more comprehensible. Rewritten mouse control routine to accept even a single eye . Solved a bug for absolute positions tracker.

Application.py
@@ -46,32 +46,42 @@ class Home:
         X_l = build_unknown_array(x_l)
         X_r = build_unknown_array(x_r)
        
-        iris_tracker = iris_position_tracker(settings.camera,err_abs=50,nr_samples_per_read=3, err_rel=30, debug=True)
+        iris_tracker = iris_position_tracker(settings.camera,nr_samples_per_read=5,err_abs=6.5, err_rel=6.5, debug=False)
         
-
-        for  i in range(100):
+        gaze_r = None
+        gaze_l = None
+        for _ in range(100):
             abs_l, abs_r, rel_l, rel_r = next(iris_tracker)
-            while any(e is None for e in [abs_l, abs_r]):
+            while all(e is None for e in [rel_l, rel_r]):
             
                 abs_l, abs_r, rel_l, rel_r = next(iris_tracker)
 
-            v_l = abs_l
-            v_r = abs_r        
+            v_l , v_r = rel_l, rel_r      
             print(v_l, v_r)
-
-            iris_l_param = build_iris_param_array(v_l)
-            iris_r_param = build_iris_param_array(v_r)
+            if v_l is not None:
+                iris_l_param = build_iris_param_array(v_l)
+                gaze_l = X_l.dot(iris_l_param)
+            if v_r is not None:
+                iris_r_param = build_iris_param_array(v_r)   
+                gaze_r = X_r.dot(iris_r_param)
             
-            gaze_l = X_l.dot(iris_l_param)
-            gaze_r = X_r.dot(iris_r_param)
-            gaze = (((gaze_l+gaze_r)/2))
-            if(np.all(gaze >=0)):
+
+            if v_l is not None and v_r is not None:
+                gaze = (((gaze_l+gaze_r)/2))
+            elif v_l is not None:
+                gaze = gaze_l
+            elif v_r is not None:
+                gaze = gaze_r
+            else:
+                gaze = None
+
+            if(gaze is not None and np.all(gaze >=0)):
                 mouse.move(gaze[0], gaze[1], absolute=True, duration=0)
-            print('Left: ' , gaze_r)
-            print('Right: ', gaze_l)
-            print('Avg:' , gaze)
+                print('Left: ' , gaze_r)
+                print('Right: ', gaze_l)
+                print('Avg:'   , gaze)
 
-            
+        iris_tracker.close()    
 
     def butnew(self, text, number, _class):
         tk.Button(self.frame, text=text,
@@ -133,20 +143,20 @@ class Calibration:
         self.explanation.insert(tk.END, f"You have pressed {id_button}th point!\n Calibration started!")
         
     
-        iris_tracker = iris_position_tracker(settings.camera,err_abs=40, err_rel=20, debug=True)
+        iris_tracker = iris_position_tracker(settings.camera,nr_samples_per_read=8,err_abs=6.5, err_rel=6.5, debug=False)
         abs_l, abs_r, rel_l, rel_r = next(iris_tracker)
-        while any(e is None for e in [abs_l, abs_r]):
+        while any(e is None for e in [rel_l, rel_r ]):
            
             abs_l, abs_r, rel_l, rel_r = next(iris_tracker)
 
-        v_l = abs_l
-        v_r = abs_r        
+        v_l , v_r = rel_l, rel_r
+              
         print(v_l, v_r)
         calibration_eye_point_left.append(np.array([1+v_l[1]**2, v_l[0], v_l[1], v_l[0]*v_l[1], v_l[0]**2, 0, 0, 0, 0, 0]))
         calibration_eye_point_left.append(np.array([1, 0, 0, 0, 0, v_l[0], v_l[1], v_l[0]*v_l[1], v_l[0]**2, v_l[1]**2]))
         calibration_eye_point_right.append(np.array([1+v_r[1]**2, v_r[0], v_r[1], v_r[0]*v_r[1], v_r[0]**2, 0, 0, 0, 0, 0]))
         calibration_eye_point_right.append(np.array([1, 0, 0, 0, 0, v_r[0], v_r[1], v_r[0]*v_r[1], v_r[0]**2, v_r[1]**2]))
-
+        iris_tracker.close()
         if(id_button < 9):
             self.explanation.delete('1.0', tk.END)
             self.explanation.insert(tk.END, f"Click on the {id_button+1}th point!")            #consider to change the visual appearance of the dot to give a feedback  to user